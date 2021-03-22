bfbcca79b04796c5d0aef975c5927345c7f4483b
Suman Gole <60792767+golesuman@users.noreply.github.com>
2021-03-22 21:17:14 +0545

    Add files via upload

--- su.py
+++ su.py
@@ -6,10 +6,34 @@ def Canny(image):
     blur = cv.GaussianBlur(gray, (5, 5), 0)
     cany = cv.Canny(blur, 50, 150)
     return cany
+def display_lines(image,lines):
+    line_image=np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2=line.reshape(4)
+            cv.line(line_image,(x1,y1),(x2,y2),(0,255,0),10)
+    return line_image
+def region_interest(image):
+    height=image.shape[0]
+    polygons=np.array([[(200,height),(1100,height),(500,250)]])
+    mask=np.zeros_like(image)
+    cv.fillPoly(mask,polygons,255)
+    masked_image=cv.bitwise_and(image,mask)
+    return masked_image
 img=cv.imread("/home/suman/PycharmProjects/suman/test.jpg")
 lane_image=np.copy(img)
 cany= Canny(lane_image)
-plt.imshow(cany)
-plt.show()
+# cv.namedWindow('cany', cv.WINDOW_NORMAL)
+# cv.resizeWindow('cany',1000,800)
+# cv.imshow('cany',cany)
+theline=region_interest(cany)
+
+lines=cv.HoughLinesP(theline,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
+line_image=display_lines(lane_image,lines)
+combo=cv.addWeighted(lane_image,0.8,line_image,1,1)
+cv.imshow('Result',combo)
+# plt.imshow(cany)
+#
+# plt.show()
 cv.waitKey(0)
 cv.destroyWindow()
